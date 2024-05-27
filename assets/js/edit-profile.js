70b48ea56f92237ce029c87daf5a582052545098
Amirudin-fahmi <amirudinfahmi27@gmail.com>
2024-05-27 23:39:37 +0700

    Aplikasi Penjemputan Sampah

---
+++ assets/js/edit-profile.js
@@ -0,0 +1,34 @@
+function toggleMenu() {
+  document.querySelector(".sub-menu-wrap").classList.toggle("open-menu");
+}
+function toggleMenuNotif() {
+  document.querySelector(".sub-menu-notif").classList.toggle("open-menu");
+}
+function changeActive(element) {
+  var links = document.querySelectorAll(".navbar ul li a");
+  links.forEach(function (link) {
+    link.classList.remove("active");
+  });
+  element.classList.add("active");
+}
+let img_logo = document.querySelector(".logo span");
+img_logo.addEventListener("click", function () {
+  window.location.href = "landingPage.html";
+});
+let tentang = document.querySelector(".tentang");
+tentang.addEventListener("click", function () {
+  window.location.href = "tentang.html";
+});
+
+function logout() {
+  var confirmDialog = document.getElementById("confirmDialog");
+  confirmDialog.style.display = "block";
+
+  document.getElementById("confirmButton").onclick = function () {
+    window.location.href = "index.html";
+  };
+
+  document.getElementById("cancelButton").onclick = function () {
+    window.location.href = "edit-profile.html";
+  };
+}
---
+++ assets/js/sub-blog-guest.js
@@ -0,0 +1,21 @@
+function toggleMenu() {
+  document.querySelector(".sub-menu-wrap").classList.toggle("open-menu");
+}
+function toggleMenuNotif() {
+  document.querySelector(".sub-menu-notif").classList.toggle("open-menu");
+}
+function changeActive(element) {
+  var links = document.querySelectorAll(".navbar ul li a");
+  links.forEach(function (link) {
+    link.classList.remove("active");
+  });
+  element.classList.add("active");
+}
+let img_logo = document.querySelector(".logo span");
+img_logo.addEventListener("click", function () {
+  window.location.href = "index.html";
+});
+let tentang = document.querySelector(".tentang");
+tentang.addEventListener("click", function () {
+  window.location.href = "tentang.html";
+});
